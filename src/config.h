5ee6cd26400a26e2a5277da9d4b9f6cb8f0ffa9c
Daniel <daniel2468114@gmail.com>
2024-11-05 14:52:15 +0000

    config file ,test verbosity

---
+++ src/config.h
@@ -0,0 +1,13 @@
+// config.h
+#pragma once
+
+#include <cstddef>
+
+// Vvtbi file extension.
+constexpr char VVTBI_EXTENSION_LITERAL[] = "vvtb";
+
+// The maximum length of string literals.
+constexpr std::size_t VVTBI_STRING_LITERAL = 50;
+
+// The maximum length of whole number literals.
+constexpr std::size_t VVTBI_NUMBER_LITERAL = 8;
